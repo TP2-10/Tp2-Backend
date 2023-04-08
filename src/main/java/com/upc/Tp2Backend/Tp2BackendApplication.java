29c5358f87c6b3805ac83ad67abdc23130e487e7
Erick.BH <erickalonsobh@gmail.com>
2023-04-07 19:53:35 -0500

    Update Tp2BackendApplication.java
    Added CORS configuration

--- src/main/java/com/upc/Tp2Backend/Tp2BackendApplication.java
+++ src/main/java/com/upc/Tp2Backend/Tp2BackendApplication.java
@@ -2,12 +2,25 @@ package com.upc.Tp2Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Tp2BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp2BackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
